fc13b6ea9751377934ffe61f5cfc0884db274695
OyunominJuno <oyunominmun@gmail.com>
2020-07-18 13:54:59 -0700

    Navbar first version
    Need to load the profile picture instead of the yellow circle and need to add functionalities

--- src/Components/Icon/map.js
+++ src/Components/Icon/map.js
@@ -1,5 +1,11 @@
 import BANANA_SVG from '../../Image/BANANA.svg';
+import ALERTBELL_SVG from '../../Image/ALERTBELL.svg';
+import TASKS_SVG from  '../../Image/TASKS.svg';
+import ELLIPSE_SVG from '../../Image/ELLIPSE.svg';
 
 export default {
   bananaIcon: BANANA_SVG,
+  alertBell: ALERTBELL_SVG,
+  tasks: TASKS_SVG,
+  ellipse: ELLIPSE_SVG
 };
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from '../Input/style.module.css';
+import styles from './style.module.css';
+import { Nav, NavItem, NavLink } from 'reactstrap';
+import Icon from '../Icon';
 
 function Navbar(props) {
   // eslint-disable-next-line no-unused-vars
   const { showMenu, showNotification } = props;
+  //const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  //const toggle = () => setDropdownOpen(!dropdownOpen);
+
   return (
-    <div className={styles.container}>Navbar</div>
+    <div className={styles.container}>
+      <Nav pills>
+        <NavItem>
+        <Icon name="bananaIcon" size={75} />
+        </NavItem>
+        <NavItem>
+          <NavLink href="#" active className={styles.bananaPortalNav}>BANANA PORTAL</NavLink>
+        </NavItem>
+        <NavItem className={styles.alertBell}>
+          <Icon name="alertBell" size={35} />
+        </NavItem>
+        <NavItem className={styles.tasks}>
+          <Icon name="tasks" size={35} />
+        </NavItem>
+        <NavItem className={styles.ellipse}>
+          <Icon name="ellipse" size={35} />
+        </NavItem>
+      </Nav>
+    </div>
   );
 }
 
