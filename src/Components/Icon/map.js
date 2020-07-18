9dc0f26a8ec59705f3cf1276dda6ce71fe44b6e8
Sean Deneen <scdeneen@gmail.com>
2020-07-18 15:36:54 -0700

    Added application cards for donors and clients.

--- src/Components/Icon/map.js
+++ src/Components/Icon/map.js
@@ -1,5 +1,10 @@
 import BANANA_SVG from '../../Image/BANANA.svg';
+import CLIENT_PLUS_SVG from '../../Image/CLIENT_PLUS.svg';
+import DONOR_PLUS_SVG from '../../Image/DONOR_PLUS.svg';
+
 
 export default {
   bananaIcon: BANANA_SVG,
+  clientPlusIcon: CLIENT_PLUS_SVG,
+  donorPlusIcon: DONOR_PLUS_SVG,
 };
