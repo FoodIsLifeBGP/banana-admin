e4758bee0138a97b6965e5ba81a4229252dbbb49
asengelhart <asengelhart@yahoo.com>
2020-08-19 22:18:29 -0600

    initial input component in coreui

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-// eslint-disable-next-line no-unused-vars
+import {
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText
+} from '@coreui/react';
+import {CIcon} from '@coreui/icons-react';
 import PropTypes from 'prop-types';
 import styles from './style.module.css';
 
 function Input(props) {
   // eslint-disable-next-line no-empty-pattern
-  const {} = props;
+  const {iconName, id, name, placeholder, type} = props;
   return (
-    <div className={styles.container}>Input</div>
+    <div className={styles.container}>
+      <CInputGroup>
+        <CInputGroupPrepend>
+          <CInputGroupText>
+            <CIcon name={iconName} />
+          </CInputGroupText>
+        </CInputGroupPrepend>
+        <CInput
+          type={type || "text"}
+          id={id}
+          name={name}
+          placeholder={placeholder}
+        ></CInput>
+      </CInputGroup>
+    </div>
   );
 }
 
 Input.propTypes = {
+  iconName: PropTypes.string,
+  placeholder: PropTypes.string
 };
 
 Input.defaultProps = {
