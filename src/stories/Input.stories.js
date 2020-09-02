c23a35d4743873ea76c0ac247c6c3899b7ccc60f
asengelhart <asengelhart@yahoo.com>
2020-09-02 01:34:46 -0600

    updating stories

--- src/stories/Input.stories.js
+++ src/stories/Input.stories.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Input from '../Components/Input';
+import M from 'materialize-css';
 
 export default {
   title: 'Input',
   component: Input,
 };
 
-export const presentation = () => <Input />;
+export const presentation = () => {
+  // M.AutoInit();
+  return <Input iconName={"person"} placeholder={"Username"} />;
+}
