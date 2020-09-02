71984afe5bf1433ed28d6d78f6373e30b5d356de
asengelhart <asengelhart@yahoo.com>
2020-09-02 01:33:55 -0600

    Input component and styling first pass

--- src/Components/Input/index.js
+++ src/Components/Input/index.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import {
-  CInputGroup,
-  CInputGroupPrepend,
-  CInputGroupText
-} from '@coreui/react';
-import {CIcon} from '@coreui/icons-react';
+  Col,
+  Row
+} from 'react-materialize';
 import PropTypes from 'prop-types';
-import styles from './style.module.css';
+import styles from './style.module.scss';
 
 function Input(props) {
   // eslint-disable-next-line no-empty-pattern
   const {iconName, id, name, onChange, placeholder, type} = props;
+  console.log(styles.icon);
+  console.log(styles.container);
   return (
-    <div className={styles.container}>
-      <CInputGroup>
-        <CInputGroupPrepend>
-          <CInputGroupText>
-            <CIcon name={iconName} />
-          </CInputGroupText>
-        </CInputGroupPrepend>
-        <CInput
-          type={type || "text"}
-          id={id}
-          name={name}
-          placeholder={placeholder}
-          onChange={onChange}
-        ></CInput>
-      </CInputGroup>
-    </div>
+    <Row>
+      <Col l={8}>
+        <div className={`input-field ${styles.container}`}>
+          <i className={`${styles.icon} material-icons prefix`}>{iconName}</i>
+          <input id={id} name={name} onChange={onChange} placeholder={placeholder} type={type || "text"} />
+        </div>
+      </Col>
+    </Row>
   );
 }
 
