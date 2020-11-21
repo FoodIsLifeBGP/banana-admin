f3d2f697a4900b1fc4bd716c27cbae1f3cd41938
Ben Beekman <bbeekman@gmail.com>
2020-11-21 13:21:46 -0800

    breadcrumb data structure and propTypes

--- src/Components/Breadcrumbs/index.js
+++ src/Components/Breadcrumbs/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { Link } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 import styles from './style.module.css';
 
-const Breadcrumbs = (props) => (
-  <div className={styles.Breadcrumbs}>
-    <div className={styles.Breadcrumb}>
-      Crumb
-    </div>
-    <div className={styles.Breadcrumb}>
-      Crumb
-    </div>
+const Breadcrumbs = (props) => {
+
+  const makeCrumbs = (crumbs) => {
+    if (crumbs.length) {
+      return crumbs.map = (crumb => {
+        return (
+          <Link key={i} to={`/${crumb.path}`}>
+            {crumbs.title}
+          </Link>
+          </>
+          );
+        })
+      }
+    };
+    
+return (
+  <div className={styles.Breadcrumbs} aria-label="breadcrumb">
+    <Link to="/" className={styles.Breadcrumb}>
+      Home
+        </Link>
+
+    {makeCrumbs}
   </div>
 );
+};
 
+Breadcrumbs.propTypes = {
+  crumbs: PropTypes.shape({
+    path: PropTypes.string,
+    label: PropTypes.string,
+  }).isRequired,
+};
 
 export default Breadcrumbs;
