b2297aeaf03dd8d83b5d46f5cd1ef0b55f14ee4d
gyermich <yermichevagalina@gmail.com>
2020-11-21 12:45:28 -0800

    Rename component ReviewApplication -> ApplicationReviewCard

---
+++ src/stories/ApplicationReviewCard.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ApplicationReviewCard from "../Components/ApplicationReviewCard";
+
+export default {
+  title: "ApplicationReviewCard",
+  component: ApplicationReviewCard,
+};
+
+export const donor = () => <ApplicationReviewCard type='donor' />;
--- src/stories/ReviewApplication.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReviewApplication from "../Components/ReviewApplication";
-
-export default {
-  title: "ReviewApplication",
-  component: ReviewApplication,
-};
-
-export const donor = () => <ReviewApplication type='donor' />;
