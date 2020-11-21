69942ef15ce974e576113cd7a5e9af7cabc6e1f2
Ben Beekman <bbeekman@gmail.com>
2020-11-21 12:28:41 -0800

    basic breadcrumb skeleton

---
+++ src/Components/Breadcrumbs/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './style.module.css';
+
+const Breadcrumbs = (props) => (
+  <div className={styles.Breadcrumbs}>
+    <div className={styles.Breadcrumb}>
+      Crumb
+    </div>
+    <div className={styles.Breadcrumb}>
+      Crumb
+    </div>
+  </div>
+);
+
+
+export default Breadcrumbs;
