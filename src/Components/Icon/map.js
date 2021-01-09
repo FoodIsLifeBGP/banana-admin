06af09eff511effaebf3214dec78c1934a8f096f
KHCode <hillkri@oregonstate.edu>
2021-01-09 15:30:45 -0800

    built and styled donation component

--- src/Components/Icon/map.js
+++ src/Components/Icon/map.js
@@ -5,6 +5,7 @@ import DONOR_PLUS_SVG from '../../Image/DONOR_PLUS.svg';
 import ALERTBELL_SVG from '../../Image/ALERTBELL.svg';
 import TASKS_SVG from '../../Image/TASKS.svg';
 import ELLIPSE_SVG from '../../Image/ELLIPSE.svg';
+import VEGGIES_SVG from '../../Image/VEGGIES.svg';
 
 export default {
   bananaIcon: BANANA_SVG,
@@ -13,4 +14,5 @@ export default {
   alertBell: ALERTBELL_SVG,
   tasks: TASKS_SVG,
   ellipse: ELLIPSE_SVG,
+  veggies: VEGGIES_SVG,
 };
