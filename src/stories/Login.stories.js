e0a6d9c57311c9ef017828c09325eaedef89a9cf
Chris Tack <chris_tack@mac.com>
2021-11-01 20:45:05 -0700

    Create Login story

---
+++ src/stories/Login.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import LoginPage from '../Pages/LoginPage';
+
+export default {
+  title: 'Login Page',
+  component: LoginPage,
+};
+
+export const loginPage = () => <LoginPage />;
