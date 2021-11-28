c8949d19e0bc4f6cf3b40ad534f2872c3d7ca7cd
Chris Tack <chris_tack@mac.com>
2021-11-28 14:11:17 -0800

    Create route for Login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+	BrowserRouter as Router,
+	Switch,
+	Route,
 } from 'react-router-dom';
 import styles from './App.module.css';
 import HomePage from './Pages/HomePage';
 import SettingsPage from './Pages/Settings';
-import LoginPage from './Pages/LoginPage';
+import LoginPage from './Pages/LoginPage/index.js';
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/settings" component={SettingsPage} />
-            <Route exact path="/login" component={LoginPage} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+	return (
+		<div className={styles.App}>
+			<Router>
+				<div>
+					<Switch>
+						<Route exact={true} path="/" component={LoginPage} />
+						<Route exact={true} path="/home" component={HomePage} />
+						<Route exact={true} path="/settings" component={SettingsPage} />
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
