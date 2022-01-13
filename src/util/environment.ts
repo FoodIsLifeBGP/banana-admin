1b35930e7bdcb087ae15ecb2ed7f41e17ba4d9cf
Chris Tack <chris_tack@mac.com>
2022-01-13 12:32:51 -0800

    Update environment

--- src/util/environment.ts
+++ src/util/environment.ts
@@ -1,18 +1,22 @@
-const ADMIN = {
-  LOGIN_URL: '/admin_auth',
-  CREATE_URL: 'admin/create',
-};
-
 const getEnv = () => {
   return ({
-    USER_IDENTITY: ADMIN,
-    LOGIN_URL: ADMIN,
-    API_BASE_URL: getServerEndpoint(),
+    CREATE_URL: 'admin_auth',
+    LOGIN_URL: '/login',
+    // TODO: Update logic for user identity to either: 'view-only' | 'admin' | 'super-admin'
+    USER_IDENTITY: 'view-only',
+    API_BASE_URL: getServerEndPoint(),
   })
 }
 
-function getServerEndpoint() {
-  return 'https://api.bananaapp.org';
+const getServerEndPoint = () => {
+  if (process.env.APP_VARIANT === "development") {
+    return 'https://dev.bananaapp.org'
+  } else if (process.env.APP_VARIANT === "production") {
+    return 'https://api.bananaapp.org';
+  } else {
+    console.error('Please set your APP_VARIANT in your .env as either "development" or "production".')
+		return
+  }
 }
 
 export default getEnv;
