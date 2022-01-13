f596ba2ecc6a43b43aa46d7e8f7365c9a3b20945
Chris Tack <chris_tack@mac.com>
2022-01-13 12:36:11 -0800

    Add logic for login component

--- src/Pages/LoginPage/index.js
+++ src/Pages/LoginPage/index.js
@@ -11,19 +11,45 @@ import Input from '../../Components/Input/index.js';
 import Icon from '../../Components/Icon';
 import styles from './style.module.scss';
 
-export default function LoginPage() {
+export default function LoginPage(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleEmailChange = (e) => {
+	  setEmail(e.target.value)
+  }
+  const handlePasswordChange = (e) => {
+	  setPassword(e.target.value)
+  }
+
   const onSubmit = (e) => {
     // TODO: build login functionality
+	e.preventDefault()
+	fetch(`https://localhost:3000/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify({
+			email, password
+		})
+	})
+	.then(res => res.json())
+	.then(data => {
+		localStorage.setItem("token", data.jwt)
+		props.handleLogin(data.user)
+	})
+
+	setEmail("")
+	setPassword("")
   };
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.borderspace} />
 			<div className={styles.mainspace}>
-				<Container className="h-100 row align-items-center">
+				<Container className="h-100 align-items-center d-flex justify-content-center">
 					<Row>
 						<Col lg={10} className="mx-auto">
 							<Row className={styles.titlerow}>
@@ -43,6 +69,7 @@ export default function LoginPage() {
 									className={styles.inputrow}
 									value={email}
 									setValue={setEmail}
+									onChange={handleEmailChange}
 								/>
 								<Input
 									id="password"
@@ -50,10 +77,10 @@ export default function LoginPage() {
 									iconName="lock"
 									placeholder="Password"
 									type="password"
-									onChange={setPassword}
 									className={styles.inputrow}
 									value={password}
 									setValue={setPassword}
+									onChange={handlePasswordChange}
 								/>
 								{/* TODO: allow Input to return a Button object */}
 								<div className="mt-5 mx-5 row row-cols-2 formSubmit">
