1555be6c33ec3e443105ec6c2ac21b31a555e7a6
AJ <ancheetah@users.noreply.github.com>
2022-01-15 15:47:16 -0500

    new applications cards

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import {
+  Container,
+  Col,
+  Row,
+} from 'reactstrap';
 import Navbar from '../../Components/Navbar';
+import ApplicationCard from '../../Components/ApplicationCard';
+import styles from './HomePage.module.css';
 
 export default function HomePage() {
   return (
-    <div>
+    <div className={styles.HomePage}>
       <Navbar />
-      <h1>
-        welcome to admin portal!
-      </h1>
+      <Container>
+        <Row>
+          <Col className="applications">
+            <h3>New Applications</h3>
+            <ApplicationCard type="client" userCount={25} approvedCount={15} />
+            <ApplicationCard type="donor" userCount={7} approvedCount={5} />
+          </Col>
+          <Col className="donation">
+            <h3>Donation Status</h3>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
