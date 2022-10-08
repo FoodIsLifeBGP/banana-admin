b64a2d6bf98ac9449bfbd267ed12a6b47728c1fb
Chris Tack <chris_tack@mac.com>
2022-10-08 11:27:36 -0700

    Revert spacing from tabs to spaces

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from 'react-router-dom';
 import styles from './App.module.css';
 import HomePage from './Pages/HomePage';
 import SettingsPage from './Pages/Settings';
-import LoginPage from './Pages/LoginPage/index.js';
+import LoginPage from './Pages/LoginPage/index';
 
 function App() {
-	return (
-		<div className={styles.App}>
-			<Router>
-				<div>
-					<Routes>
-						<Route exact={true} path="/" element={<LoginPage />} />
-						<Route exact={true} path="/home" element={<HomePage />} />
-						<Route exact={true} path="/settings" element={<SettingsPage />} />
-					</Routes>
-				</div>
-			</Router>
-		</div>
-	);
+  return (
+    <div className={styles.App}>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<LoginPage />} />
+            <Route exact path="/home" element={<HomePage />} />
+            <Route exact path="/settings" element={<SettingsPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
