fe5fb0d0b76c0402648975718fa86adb08d848ed
lofiandcode <josephtrettevik@gmail.com>
2023-01-28 15:20:06 -0800

    Create NotificationCard component template
    Create NotificationCard folder in Components folder. Create index.js and style.module.css files in NotificationCard folder. Create React template for NotificationCard in index.js.

---
+++ src/Components/NotificationCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './style.module.css';
+
+function NotificationCard(props) {
+  return (
+    <div>
+      NotificationCard
+    </div>
+  );
+}
+
+export default NotificationCard;
