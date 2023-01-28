cae31cfaca26aae59011beeaa0af86855490b946
lofiandcode <josephtrettevik@gmail.com>
2023-01-28 15:24:35 -0800

    Create NotificationCard.stories.js file in stories folder

---
+++ src/stories/NotificationCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NotificationCard from '../Components/NotificationCard';
+
+export default {
+  title: 'NotificationCard',
+  component: NotificationCard,
+};
+
+export const presentation = () => <NotificationCard />;
