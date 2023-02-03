33e628e15190f2402d13bc620ea0bd420fc6a070
Ren Burnett <ren.burnett@mobyinc.com>
2023-02-02 19:22:46 -0800

    update port, avoids local running backend conflict

--- src/util/environment.js
+++ src/util/environment.js
@@ -4,7 +4,7 @@ const getServerEndPoint = () => {
   } if (process.env.REACT_APP_VARIANT === 'production') {
     return 'https://api.bananaapp.org';
   } if (process.env.REACT_APP_VARIANT === 'local') {
-    return 'http://localhost:3000';
+    return 'http://localhost:3004';
   }
   // eslint-disable-next-line no-console
   return console.error('Please set your APP_VARIANT in your .env as either "development", "production" or "local".');
