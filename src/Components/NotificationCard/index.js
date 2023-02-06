5c9ed51c5fcf8c509887c5b27db3cfaac557ddeb
lofiandcode <josephtrettevik@gmail.com>
2023-02-06 15:45:27 -0800

    Import ProfilePicture component

--- src/Components/NotificationCard/index.js
+++ src/Components/NotificationCard/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-// import styles from './style.module.css';
+import styles from './style.module.css';
+import ProfilePicture from '../ProfilePicture';
 
 function NotificationCard() {
   return (
     <div>
-      NotificationCard
+      <ProfilePicture srcImage="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+      <div className={styles.notification}>Notification</div>
     </div>
   );
 }
