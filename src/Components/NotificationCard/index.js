a008a0f9154d95458f9b8a3da054b7118549a326
lofiandcode <josephtrettevik@gmail.com>
2023-02-09 19:39:44 -0800

    Use Reactstrap to create rows and columns of layout

--- src/Components/NotificationCard/index.js
+++ src/Components/NotificationCard/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 // import PropTypes from 'prop-types';
 import styles from './style.module.css';
 import ProfilePicture from '../ProfilePicture';
 
 function NotificationCard() {
   return (
-    <div>
-      <ProfilePicture srcImage="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-      <div className={styles.notification}>Notification</div>
-    </div>
+    <Container className="container container-fluid">
+      <Row className="notification-container">
+        <Col className="profile-container" xs="2">
+          <ProfilePicture srcImage="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        </Col>
+        <Col className="d-flex flex-column justify-content-center" xs="10">
+          <Row className={styles.notification}>
+            Admin 1 reviewed Zach Gallaways application as APPROVED.
+          </Row>
+          <Row className={styles.date}>
+            11:30 am.July 5, 2020
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
