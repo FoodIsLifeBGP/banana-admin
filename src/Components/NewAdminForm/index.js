3eda74f0bc6707c630abac184243f4d523030cf4
Tim <timtran007@gmail.com>
2023-03-18 13:16:15 -0500

    swaps input for Input component

--- src/Components/NewAdminForm/index.js
+++ src/Components/NewAdminForm/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import Input from '../Input';
 import styles from './style.module.css';
 import Icon from '../Icon';
 
@@ -20,7 +21,7 @@ function NewAdminForm() {
 
   function handleOnChange(e) {
     const key = e.target.name;
-    const value = e.target.value;
+    const { value } = e.target;
     setFormData({
       ...formData,
       [key]: value,
@@ -28,55 +29,75 @@ function NewAdminForm() {
   }
 
   return (
-    <div className={styles.container}>
+    <div >
       <h3>Add New Admin</h3>
-      <form className={styles.newAdminForm}>
+      <form>
         <div className={styles.fieldContainer}>
-          <label htmlFor="firstName">First name</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="firstName"
-            placeholder="Enter first name"
-            value={formData.firstName}
-          />
+          <label htmlFor="firstName">
+            <div>First name</div>
+            <Input
+              iconName=''
+              id="firstName"
+              onChange={handleOnChange}
+              type="text"
+              name="firstName"
+              placeholder="Enter first name"
+              value={formData.firstName}
+            />
+          </label>
         </div>
         <div className={styles.fieldContainer}>
-          <label htmlFor="lastName">Last name</label>
-          <input
-            onChange={handleOnChange}
-            type="text"
-            name="lastName"
-            placeholder="Enter last name"
-            value={formData.lastName} />
+          <label htmlFor="lastName">
+            <div>Last name</div>
+            <Input
+              iconName=''
+              id="lastName"
+              onChange={handleOnChange}
+              type="text"
+              name="lastName"
+              placeholder="Enter last name"
+              value={formData.lastName}
+            />
+          </label>
         </div>
         <div className={styles.fieldContainer}>
-          <label htmlFor="email">Email</label>
-          <input
-            onChange={handleOnChange}
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={formData.email} />
+          <label htmlFor="email">
+            <div>Email</div>
+            <Input
+              iconName=''
+              id="email"
+              onChange={handleOnChange}
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={formData.email}
+            />
+          </label>
         </div>
         <div className={styles.fieldContainer}>
-          <label htmlFor="password">Enter Password </label>
-          <input
-            onChange={handleOnChange}
-            type={showPassword ? 'text' : 'password'}
-            name="password"
-            placeholder="Enter Super Admin password"
-            value={formData.password} />
-          <i onClick={handlePassword}>
+          <label htmlFor="password">
+            <div>Enter Password</div>
+            <Input
+              iconName=''
+              id="password"
+              onChange={handleOnChange}
+              type={showPassword ? 'text' : 'password'}
+              name="password"
+              placeholder="Enter Super Admin password"
+              value={formData.password}
+            />
+          </label>
+          <radio onClick={handlePassword} onKeyDown={() => {}}>
             {showPassword ? (<Icon name="visibleEye" />) : (<Icon name="hiddenEye" />) }
-          </i>
+          </radio>
         </div>
         <div className={styles.buttonContainer}>
-          <input type='submit' value='Back' id={styles.buttonBack} />
+          <input type="submit" value="Back" id={styles.buttonBack} />
           <input
             type="submit"
             value="Confirm"
-            id={styles.buttonConfirm} />
+            id={styles.buttonConfirm}
+          />
         </div>
       </form>
     </div>
