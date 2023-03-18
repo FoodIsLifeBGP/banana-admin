bde348d2d55692d2ab3abe3e36c4711606c4a592
Tim <timtran007@gmail.com>
2023-03-18 14:42:00 -0500

    adds structure for modal

---
+++ src/Components/Modal/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+function Modal() {
+  return (
+    <div className={styles.container} />
+  );
+}
+
+export default Modal;
---
+++ src/stories/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Modal from '../Components/Modal';
+
+export default {
+  title: 'Modal',
+  component: Modal,
+};
+
+export const modal = () => (
+  <Modal />
+);
