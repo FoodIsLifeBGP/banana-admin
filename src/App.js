8f1ed3a4560bdf8d7fc2b9701125768929a39b67
Chris Tack <chris_tack@mac.com>
2023-04-01 11:07:00 -0700

    Wrap protected routes

--- src/App.js
+++ src/App.js
@@ -9,17 +9,34 @@ import HomePage from './Pages/HomePage';
 import SettingsPage from './Pages/SettingsPage';
 import ReviewApplicationPage from './Pages/ReviewApplicationPage';
 import LoginPage from './Pages/LoginPage/index';
+import useGlobal from "./state";
+import PrivateRoute from "./Pages/AuthenticationWrapper";
 
 function App() {
+  const [globalState] = useGlobal();
+  const [localState, setLocalState] = React.useState(0);
+  //TODO: Remove these console.logs
+  console.log(localState);
+  console.log(globalState);
+
   return (
     <div className={styles.App}>
+      <button type="button" onClick={() => setLocalState(localState + 1)}>
+        Add 1
+      </button>
       <Router>
         <div>
           <Routes>
             <Route exact path="/" element={<LoginPage />} />
-            <Route exact path="/home" element={<HomePage />} />
-            <Route exact path="/settings" element={<SettingsPage />} />
-            <Route exact path="/review-applications" element={<ReviewApplicationPage />} />
+            <Route element={<PrivateRoute />}>
+              <Route exact path="/home" element={<HomePage />} />
+              <Route exact path="/settings" element={<SettingsPage />} />
+            </Route>
+            <Route
+              exact
+              path="/review-applications"
+              element={<ReviewApplicationPage />}
+            />
           </Routes>
         </div>
       </Router>
