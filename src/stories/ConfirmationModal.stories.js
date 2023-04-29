02433d5e448dd9dff294a91cbbf4d307135e0a9f
Tim <timtran007@gmail.com>
2023-04-29 14:03:55 -0500

    updates ConfirmationModal stories

---
+++ src/stories/ConfirmationModal.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ConfirmationModal from '../Components/ConfirmationModal';
+
+export default {
+  title: 'ConfirmationModal',
+  component: ConfirmationModal,
+};
+
+export const presentation = () => <ConfirmationModal />;
