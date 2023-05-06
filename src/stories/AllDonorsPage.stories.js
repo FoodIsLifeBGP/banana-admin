74682e4647db3225ef692c61602383a3f281ea38
Brendan Smith <brendan.smith903@gmail.com>
2023-05-06 11:53:56 -0700

    Donor page (#48)
    * feature/Donor-Application-Page
    
    * move paginate funcs to DonorP
    
    * update dev ip address
    
    * Donor page and some breadcrumb updates

---
+++ src/stories/AllDonorsPage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AllDonorsPage from '../Pages/AllDonorsPage';
+
+export default {
+  title: 'AllDonorsPage',
+  component: AllDonorsPage,
+};
+
+export const presentation = () => <AllDonorsPage />;
---
+++ src/stories/DonorPage.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import DonorPage from '../Pages/DonorPage';
+
+export default {
+  title: 'DonorPage',
+  component: DonorPage,
+};
+
+export const presentation = () => <DonorPage />;
