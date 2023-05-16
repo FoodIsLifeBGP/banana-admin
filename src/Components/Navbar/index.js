a922e67280d0153f47d1b8e3609f3ca1ff11d823
Tim <timtran007@gmail.com>
2023-05-15 22:39:03 -0500

    creates dropdown menu for lists of all, clients, donors

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Nav,
@@ -9,6 +9,10 @@ import {
   NavItem,
   Row,
   Col,
+  Dropdown,
+  DropdownItem,
+  DropdownToggle,
+  DropdownMenu,
 } from 'reactstrap';
 import styles from './style.module.css';
 import Icon from '../Icon';
@@ -16,18 +20,19 @@ import Icon from '../Icon';
 function Navbar(props) {
   // eslint-disable-next-line no-unused-vars
   const { showMenu, showNotification } = props;
-  // const [dropdownOpen, setDropdownOpen] = useState(false);
-
-  // const toggle = () => setDropdownOpen(!dropdownOpen);
 
   // Bell: /notifications
   // Banana Portal: /home
   // Profile Pic: /settings
   // List: /all-clients (or /all-donors depending on what list is more popular)
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen(!dropdownOpen);
+
   return (
     <BootstrapNavbar className={styles.container}>
-      <NavbarBrand href="/">
+      <NavbarBrand href="/home">
         <Row className="align-items-center">
           <Col>
             <Icon name="bananaIcon" size={75} />
@@ -48,7 +53,29 @@ function Navbar(props) {
           </NavLink>
         </NavItem>
         <NavItem className={styles.navIcon}>
-          <Icon name="tasks" size={35} />
+          <Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+            {dropdownOpen ? (
+              <DropdownToggle nav caret style={{ backgroundColor: '#F0EEEE' }}>
+                <Icon name="tasks" size={35} />
+              </DropdownToggle>
+            ) : (
+              <DropdownToggle nav caret>
+                <Icon name="tasks" size={35} />
+              </DropdownToggle>
+            )}
+
+            <DropdownMenu>
+              <DropdownItem>
+                <NavLink href="/users/all">All</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/users/clients">Clients</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/users/donors">Donors</NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavItem>
         <NavItem className={styles.navIcon}>
           <NavLink href="/settings">
