507bb8ad834d38dbd29499e05610a64a22148993
Tim <timtran007@gmail.com>
2023-05-15 22:38:02 -0500

    adds notifications route to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HomePage from './Pages/HomePage';
 import SettingsPage from './Pages/SettingsPage';
 import ReviewApplicationPage from './Pages/ReviewApplicationPage';
 import LoginPage from './Pages/LoginPage/index';
+import NotificationPage from './Pages/NotificationPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route exact path="/home" element={<HomePage />} />
             <Route exact path="/settings" element={<SettingsPage />} />
             <Route exact path="/review-applications" element={<ReviewApplicationPage />} />
+            <Route exact path="/notifications" element={<NotificationPage />} />
           </Routes>
         </div>
       </Router>
