d5f5c045df3bb92426702fe0084accf97075bb78
Tim <timtran007@gmail.com>
2023-08-19 16:04:23 -0500

    adds dummy data to iterate for notification cards

--- src/Pages/NotificationPage/index.js
+++ src/Pages/NotificationPage/index.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../../Components/Navbar';
 import styles from './style.module.scss';
+import logo from '../../logo.svg';
 // import Icon from '../../Components/Icon';
 
 function NotificationPage() {
   // const ladyWidth = 400;
   // const bananaWidth = 60;
+
+  // data to populate notifications
+  const notifications = [
+    {
+      profilePic: `${logo}`,
+      adminName: 'Admin 1',
+      applicantName: 'creative ',
+      status: 'DENIED - INCOMPLETE',
+      date: {
+        hour: '11',
+        minute: '30',
+        period: 'AM',
+        month: '8',
+        day: '19',
+        year: '2023',
+      },
+    },
+    {
+      profilePic: `${logo}`,
+      adminName: 'Admin 1',
+      applicantName: 'creative ',
+      status: 'DENIED - SUSPENDED',
+      date: {
+        hour: '11',
+        minute: '30',
+        period: 'AM',
+        month: '8',
+        day: '19',
+        year: '2023',
+      },
+    },
+    {
+      profilePic: `${logo}`,
+      adminName: 'Admin 1',
+      applicantName: 'creative ',
+      status: 'CLOSED',
+      date: {
+        hour: '11',
+        minute: '30',
+        period: 'AM',
+        month: '8',
+        day: '19',
+        year: '2023',
+      },
+    },
+    {
+      profilePic: `${logo}`,
+      adminName: 'Admin 1',
+      applicantName: 'creative ',
+      status: 'APPROVED',
+      date: {
+        hour: '11',
+        minute: '30',
+        period: 'AM',
+        month: '8',
+        day: '19',
+        year: '2023',
+      },
+    },
+    {
+      profilePic: `${logo}`,
+      adminName: 'Admin 1',
+      applicantName: 'creative ',
+      status: 'INACTIVE',
+      date: {
+        hour: '11',
+        minute: '30',
+        period: 'AM',
+        month: '8',
+        day: '19',
+        year: '2023',
+      },
+    },
+
+  ];
 
   return (
     <div>
