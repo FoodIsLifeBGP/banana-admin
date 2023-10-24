1be7a893138766028b10b3f518db5745e3ee3036
Chris Tack <chris_tack@mac.com>
2023-10-23 21:35:30 -0700

    Update story to render sample data

--- src/stories/BreadCrumb.stories.js
+++ src/stories/BreadCrumb.stories.js
@@ -7,5 +7,11 @@ export default {
 };
 
 export function Presentation() {
-  return <BreadCrumb />;
+  const sampleTrail = [
+    { pageName: 'Home', url: 'localhost:3000' },
+    { pageName: 'Donors', url: 'localhost:3000' },
+    { pageName: 'New Applications', url: 'localhost:3000' },
+  ];
+
+  return <BreadCrumb breadCrumbTrail={sampleTrail} />;
 }
