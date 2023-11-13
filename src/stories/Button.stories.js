bc29066b0b1f5476d5c7a7035110a499cbbc6742
Christopher Tack <chris_tack@mac.com>
2023-11-12 17:36:12 -0800

    404 page styling (#61)
    * Update <Button /> to receive variant prop. Create styles for `buttonPrimary` and `buttonSecondary`
    
    * Update nav styles
    
    * Update `Button` story to display primary and secondary buttons
    
    * Update 404 page to match Figma. Remove unused styles. Add button variant

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,11 +1,27 @@
-import React from 'react';
 import Button from '../Components/Button';
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      control: { type: 'select', options: ['buttonPrimary', 'buttonSecondary'] },
+    },
+    text: { control: 'text' },
+    onClick: { action: 'clicked' },
+  },
 };
 
-export function Presentation() {
-  return <Button text="ButtonText" />;
-}
+export const Primary = {
+  args: {
+    text: 'Primary Button',
+    variant: 'buttonPrimary',
+  },
+};
+
+export const Secondary = {
+  args: {
+    text: 'Secondary Button',
+    variant: 'buttonSecondary',
+  },
+};
