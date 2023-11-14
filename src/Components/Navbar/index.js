1850dd2c720b4e5271f413a9fe72d563604b73e5
Chris Tack <chris_tack@mac.com>
2023-11-14 09:03:09 -0800

    Make nav mobile responsive

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -24,62 +24,62 @@ function BananaAdminNavbar() {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className={styles.container}>
-      <Navbar expand="sm" light>
-        <NavbarBrand href="/" className={styles.navBarBrand}>
-          <Icon name="bananaIcon" size={75} className={styles.navIcon} />
-          <NavbarText className={styles.bananaPortalNav}>
-            BANANA
-            <br />
-            PORTAL
-          </NavbarText>
-        </NavbarBrand>
+    // <div className={styles.container}>
+    <Navbar expand="sm" light className={styles.container}>
+      <NavbarBrand href="/" className={styles.navBarBrand}>
+        <Icon name="bananaIcon" size={75} className={styles.navIcon} />
+        <NavbarText className={styles.bananaPortalNav}>
+          BANANA
+          <br />
+          PORTAL
+        </NavbarText>
+      </NavbarBrand>
 
-        <NavbarToggler onClick={toggle} />
+      <NavbarToggler onClick={toggle} />
 
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className={`ms-auto + ${styles.mobileNav}`} navbar>
-            <NavItem>
-              <NavLink href="/notifications">
-                <Icon name="alertBell" size={32} className={styles.navIcon} />
-                <NavbarText className={`${styles.displayMobile} + ${styles.navbarText}`}>
-                  Notifications
-                </NavbarText>
-              </NavLink>
-            </NavItem>
-
-            <UncontrolledDropdown nav inNavbar className={styles.hideMobile}>
-              <DropdownToggle
-                nav
-                caret
-                className={`${styles.dropdownToggleMobile} + ${styles.navbarText}`}
-              >
-                <Icon name="tasks" size={32} className={styles.navIcon} />
-                <NavbarText className={styles.displayMobile}>Tasks</NavbarText>
-              </DropdownToggle>
-              <DropdownMenu right className={styles.mobileDropdownTasks}>
-                <DropdownItem>
-                  <NavLink href="/all">All</NavLink>
-                </DropdownItem>
-                <DropdownItem>
-                  <NavLink href="/clients">Clients</NavLink>
-                </DropdownItem>
-                <DropdownItem>
-                  <NavLink href="/donors">Donors</NavLink>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-
-            <NavLink href="/settings">
-              <Icon name="avatar" size={35} className={styles.navIcon} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className={`ms-auto + ${styles.mobileNav}`} navbar>
+          <NavItem>
+            <NavLink href="/notifications">
+              <Icon name="alertBell" size={32} className={styles.navIcon} />
               <NavbarText className={`${styles.displayMobile} + ${styles.navbarText}`}>
-                Profile
+                Notifications
               </NavbarText>
             </NavLink>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
+          </NavItem>
+
+          <UncontrolledDropdown nav inNavbar className={styles.hideMobile}>
+            <DropdownToggle
+              nav
+              caret
+              className={`${styles.dropdownToggleMobile} + ${styles.navbarText}`}
+            >
+              <Icon name="tasks" size={32} className={styles.navIcon} />
+              <NavbarText className={styles.displayMobile}>Tasks</NavbarText>
+            </DropdownToggle>
+            <DropdownMenu right className={styles.mobileDropdownTasks}>
+              <DropdownItem>
+                <NavLink href="/all">All</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/clients">Clients</NavLink>
+              </DropdownItem>
+              <DropdownItem>
+                <NavLink href="/donors">Donors</NavLink>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+
+          <NavLink href="/settings">
+            <Icon name="avatar" size={35} className={styles.navIcon} />
+            <NavbarText className={`${styles.displayMobile} + ${styles.navbarText}`}>
+              Profile
+            </NavbarText>
+          </NavLink>
+        </Nav>
+      </Collapse>
+    </Navbar>
+    // </div>
   );
 }
 
