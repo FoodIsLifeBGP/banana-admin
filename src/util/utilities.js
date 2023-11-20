72ea0573d5858743c568227952f811cb665eb8ca
Chris Tack <chris_tack@mac.com>
2023-11-19 22:24:34 -0800

    Create Utilities file

---
+++ src/util/utilities.js
@@ -0,0 +1,19 @@
+const formatDateToPST = (dateString) => {
+  const date = new Date(dateString);
+
+  // Format the date to PST (UTC -8 hours)
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false, // Use 24-hour format
+    timeZone: 'America/Los_Angeles',
+  };
+
+  // Return formatted string
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+};
+
+export default formatDateToPST;
