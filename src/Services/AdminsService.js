80d2404850baceff06d10bcaa0a6de2e3fa89ab3
Chris Tack <chris_tack@mac.com>
2023-11-26 21:35:36 -0800

    Create `AdminsService.js`

---
+++ src/Services/AdminsService.js
@@ -0,0 +1,35 @@
+import ApiService from './ApiService';
+
+const { axiosRequest } = ApiService();
+
+const GetAdmins = async (pageNumber, pageSize) => {
+  const response = await axiosRequest('GET', `/admins?page=${pageNumber}&items=${pageSize}`);
+  return response.data;
+};
+
+const GetAdmin = async (id) => {
+  const response = await axiosRequest('GET', `/admins/${id}`);
+  return response.data.client;
+};
+
+const CreateAdmin = async (firstName, lastName, email, password) => {
+  const response = await axiosRequest('POST', '/admins/create', {
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+  return response.data;
+};
+
+const UpdateAdminStatus = async (id, status) => {
+  const response = await axiosRequest('PATCH', `clients/${id}/update_status`, {
+    id,
+    status,
+  });
+  return response.data;
+};
+
+export {
+  GetAdmins, GetAdmin, CreateAdmin, UpdateAdminStatus,
+};
