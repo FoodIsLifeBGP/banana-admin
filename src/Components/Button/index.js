01c3a35d8b90798640a5d408f67651ed0a9361ae
Chris Tack <chris_tack@mac.com>
2023-11-26 21:36:55 -0800

    Refactor `Button` component to handle additional variants

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -5,9 +5,7 @@ import styles from './style.module.css';
 function Button({
   text, style, action, variant,
 }) {
-  const buttonClass = `${styles.buttonContainer} ${
-    variant === 'buttonPrimary' ? styles.buttonPrimary : styles.buttonSecondary
-  }`;
+  const buttonClass = `${styles.buttonContainer} ${styles[variant] || styles.buttonDefault}`;
 
   return (
     <button type="button" onClick={action} className={buttonClass} style={style}>
@@ -21,7 +19,12 @@ Button.propTypes = {
   // eslint-disable-next-line react/require-default-props
   style: PropTypes.object,
   action: PropTypes.func.isRequired,
-  variant: PropTypes.string.isRequired,
+  variant: PropTypes.oneOfType([
+    'buttonPrimary',
+    'buttonSecondary',
+    'buttonDanger',
+    'buttonSuccess',
+  ]).isRequired,
 };
 
 export default Button;
