e91884186bef31857844bce8305ef6716ac01e7b
Chris Tack <chris_tack@mac.com>
2023-11-26 21:34:33 -0800

    Add `edit` and `trash` icons. Alphabetize icons and imports

--- src/Components/Icon/map.js
+++ src/Components/Icon/map.js
@@ -1,33 +1,37 @@
+import ALERTBELL_SVG from '../../Image/ALERTBELL.svg';
 import AVATAR_SVG from '../../Image/AVATAR.svg';
 import BANANA_SVG from '../../Image/BANANA.svg';
+import BANANA_PEEL_SVG from '../../Image/BANANA_PEEL.svg';
 import CLIENT_PLUS_SVG from '../../Image/CLIENT_PLUS.svg';
 import DONOR_PLUS_SVG from '../../Image/DONOR_PLUS.svg';
 import DONATION_STATUS from '../../Image/DONATION_STATUS.svg';
-import ALERTBELL_SVG from '../../Image/ALERTBELL.svg';
+import EDIT from '../../Image/EDIT.svg';
 import ELLIPSE_SVG from '../../Image/ELLIPSE.svg';
+import HIDDEN_EYE_SVG from '../../Image/HIDDEN_EYE.svg';
 import SEARCH_SVG from '../../Image/SEARCH.svg';
 import TASKS_SVG from '../../Image/TASKS.svg';
-import VEGGIES_SVG from '../../Image/VEGGIES.svg';
-import HIDDEN_EYE_SVG from '../../Image/HIDDEN_EYE.svg';
-import VISIBLE_EYE_SVG from '../../Image/VISIBLE_EYE.svg';
-import BANANA_PEEL_SVG from '../../Image/BANANA_PEEL.svg';
+import TRASH from '../../Image/TRASH.svg';
 import UPSET_PERSON_SVG from '../../Image/UPSET_PERSON.svg';
+import VEGGIES_SVG from '../../Image/VEGGIES.svg';
+import VISIBLE_EYE_SVG from '../../Image/VISIBLE_EYE.svg';
 
 const icons = {
   alertBell: ALERTBELL_SVG,
   avatar: AVATAR_SVG,
   bananaIcon: BANANA_SVG,
+  bananaPeel: BANANA_PEEL_SVG,
   clientPlusIcon: CLIENT_PLUS_SVG,
   donationStatus: DONATION_STATUS,
   donorPlusIcon: DONOR_PLUS_SVG,
+  edit: EDIT,
   ellipse: ELLIPSE_SVG,
+  hiddenEye: HIDDEN_EYE_SVG,
   search: SEARCH_SVG,
   tasks: TASKS_SVG,
-  veggies: VEGGIES_SVG,
-  hiddenEye: HIDDEN_EYE_SVG,
-  visibleEye: VISIBLE_EYE_SVG,
-  bananaPeel: BANANA_PEEL_SVG,
+  trash: TRASH,
   upsetWoman: UPSET_PERSON_SVG,
+  veggies: VEGGIES_SVG,
+  visibleEye: VISIBLE_EYE_SVG,
 };
 
 export default icons;
