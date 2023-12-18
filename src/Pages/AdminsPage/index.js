fe4309d2664a920389cf463569fef4dc33961e3c
Chris Tack <chris_tack@mac.com>
2023-12-18 14:34:57 -0800

    Remove mockAdmins and unused code

--- src/Pages/AdminsPage/index.js
+++ src/Pages/AdminsPage/index.js
@@ -9,7 +9,7 @@ import Search from '../../Components/Search';
 import Spinner from '../../Components/Spinner/Spinner';
 
 // The next line will be uncommented when the back end is ready:
-// import { GetAdmins, UpdateAdminStatus } from '../../Services/AdminsService';
+import { GetAdmins, UpdateAdminStatus } from '../../Services/AdminsService';
 
 import formatDateToPST from '../../util/utilities';
 
@@ -18,7 +18,7 @@ import styles from './style.module.scss';
 function AdminsPage() {
   const defaultPageSize = 8;
   const [admins, setAdmins] = useState([]);
-  const [sortColumn, setSortColumn] = useState({ sort_by: 'no', order: 'asc' });
+  const [sortColumn, setSortColumn] = useState({ path: 'id', order: 'asc' });
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsCount, setItemsCount] = useState(0);
   const [loading, setLoading] = useState(true);
@@ -30,83 +30,13 @@ function AdminsPage() {
 
   const navigate = useNavigate();
 
-  const mockAdmins = [
-    {
-      id: 1,
-      account_status: 'inactive',
-      created_at: '2023-10-01T01:34:45.243Z',
-      email: 'zachary@olson.com',
-      first_name: 'Naoma',
-      last_name: 'Rogahn',
-    },
-    {
-      id: 2,
-      account_status: 'active',
-      created_at: '2023-10-01T01:34:46.354Z',
-      email: 'darron@adams-harvey.name',
-      first_name: 'Jamal',
-      last_name: 'Ziemann',
-    },
-    {
-      id: 3,
-      account_status: 'suspended',
-      created_at: '2023-10-01T01:34:48.028Z',
-      email: 'elisha@gusikowski.biz',
-      first_name: 'Freeman',
-      last_name: 'Lind',
-    },
-    {
-      id: 4,
-      account_status: 'active',
-      created_at: '2023-10-01T01:34:48.620Z',
-      email: 'carley.considine@turcotte.info',
-      first_name: 'Ward',
-      last_name: 'MacGyver',
-    },
-    {
-      id: 5,
-      account_status: 'active',
-      created_at: '2023-10-01T01:34:49.217Z',
-      email: 'mario@tremblay.io',
-      first_name: 'Roland',
-      last_name: 'Anderson',
-    },
-    {
-      id: 6,
-      account_status: 'inactive',
-      created_at: '2023-10-01T01:34:49.803Z',
-      email: 'page_mann@beer.name',
-      first_name: 'Stacey',
-      last_name: 'Yundt',
-    },
-    {
-      id: 7,
-      account_status: 'suspended',
-      created_at: '2023-10-01T01:34:50.367Z',
-      email: 'willie.jakubowski@krajcik.name',
-      first_name: 'Cruz',
-      last_name: 'Cremin',
-    },
-    {
-      id: 8,
-      account_status: 'approved',
-      created_at: '2023-10-01T01:34:50.944Z',
-      email: 'jetta_abshire@wisozk.co',
-      first_name: 'Nicholas',
-      last_name: 'Heathcote',
-    },
-  ];
-
   const getAdmins = async () => {
     setLoading(true);
     try {
-      // const response = await GetAdmins();
-      // setItemsCount(response.pagy.count);
-      // const response = await GetAdmins(currentPage, defaultPageSize);
-      // setItemsCount(response.pagy.count);
-      // TODO: remove mock data
-      setAdmins(mockAdmins);
-      // setAdmins(response.data);
+      const { path, order } = sortColumn;
+      const response = await GetAdmins(currentPage, defaultPageSize, path, order);
+      setItemsCount(response.pagy.count);
+      setAdmins(response.data);
     } catch (error) {
       setItemsCount(0);
       setAdmins([]);
@@ -118,11 +48,7 @@ function AdminsPage() {
   const deactivateAdmin = async (admin) => {
     setLoading(true);
     try {
-      // TODO: uncomment the next line when the back end is ready and remove the alert
-      // await UpdateAdminStatus(admin.id, 'inactive');
-      alert(
-        `Deactivating ${admin.first_name} ${admin.last_name}...not really, but it will eventually do so once the back end routes are ready`,
-      );
+      await UpdateAdminStatus(admin.id, 'inactive');
       setLoading(false);
     } catch (error) {
       toast.error('Failed to deactivate admin');
