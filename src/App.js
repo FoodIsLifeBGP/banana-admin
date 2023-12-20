e0f822121793de248d5d4a6a7bdfd5cc9196d32a
Chris Tack <chris_tack@mac.com>
2023-12-20 08:32:17 -0800

    Wrap App in AuthContextProvider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import { AppContextProvider } from './contexts/AppContext';
+
 import Layout from './Components/Layout/index';
 import LoginPage from './Pages/LoginPage/index';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public route */}
-        <Route path="/login" element={<LoginPage />} />
+    <AppContextProvider>
+      <Router>
+        <Routes>
+          {/* Public route */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* Protected routes */}
-        <Route path="/*" element={<Layout />} />
-      </Routes>
-    </Router>
+          {/* Protected routes */}
+          <Route path="/*" element={<Layout />} />
+        </Routes>
+      </Router>
+    </AppContextProvider>
   );
 }
 
